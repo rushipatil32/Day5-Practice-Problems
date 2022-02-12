abe04602115fa39f2e1cbdab9e3db34fa9fc14c4
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-12 17:14:17 +0530

    Added:Compute quotient and reminder

---
+++ src/com/javaoroblem/QuotientReminder.java
@@ -0,0 +1,28 @@
+package com.javaoroblem;
+
+import java.util.Scanner;
+
+public class QuotientReminder {
+	
+	public static void quotientremainder() {
+		Scanner sc = new Scanner(System.in); 
+
+		System.out.println("Enter a First number :" );
+		int num1 = sc.nextInt(); 
+
+
+		System.out.println("Enter a second  number :" );
+		int num2 = sc.nextInt(); 
+
+
+		int quotient = num1 / num2;   
+		int remainder = num1 % num2;  
+
+		System.out.println("The quotient is: " + quotient);
+		System.out.println("The remainder is: " + remainder);
+		sc.close();
+	}
+	public static void main(String[] args) {
+		quotientremainder();
+	}
+}
