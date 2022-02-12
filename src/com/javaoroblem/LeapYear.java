437b8c931a33d25a8bec159fc081cbbefcfa4a67
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-12 16:35:10 +0530

    Added:

---
+++ src/com/javaoroblem/LeapYear.java
@@ -0,0 +1,28 @@
+package com.javaoroblem;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	
+	public static void leapYear() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a year to check : ");
+		int year = sc.nextInt(); 
+
+		if (year % 400 == 0) {
+			System.out.println(year + " is a leap year");
+		} else if (year % 100 == 0) {
+			System.out.println(year + " is not a leap year");
+		} else if (year % 4 == 0) {
+			System.out.println(year + " is a leap year");
+		}else  {
+			System.out.println(year + " is not a leap year");
+		}
+		sc.close();
+	}
+	public static void main(String[] args) {
+		leapYear();
+	}
+
+}
+
